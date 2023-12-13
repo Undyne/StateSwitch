5232674a051811cfe4b431cfd6c95256446f862e
RocketPlanGreg <127329462+RocketPlanGreg@users.noreply.github.com>
2023-12-13 09:24:14 -0800

    Initial Commit

---
+++ StateSwitch/StateSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateSwitchApp.swift
+//  StateSwitch
+//
+//  Created by Greg Rodrigues on 2023-12-13.
+//
+
+import SwiftUI
+
+@main
+struct StateSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
